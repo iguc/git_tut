fa210fc9bab068bff6fcf100856762c36be6c66f
iguc <iguti.yuu@gmail.com>
2020-06-27 14:19:02 +0900

    初回コミット

---
+++ sub.js
@@ -0,0 +1,10 @@
+/**
+ * サンプルメソッド
+ * @param arg1 パラメータ情報の表示
+ * @param arg2 
+ * @return 返り値情報の表示
+ */
+export function example(arg1, arg2) {
+  console.log('example');
+  return 'result: ' + arg1 + arg2;
+}
